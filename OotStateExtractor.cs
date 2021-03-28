9c07cf3d95601e7973aab6a5ddb7f23b05440ff5
Doug Parker <dgp1130@users.noreply.github.com>
2021-03-27 23:01:06 -0700

    Adds basic `Watch` implementation.
    This uses the `Watch` API to observe the upgrade memory address and log it whenever it changes. Unfortunately this requires polling at every update to look for changes, which is a bit unfortunate. I did try `IMemoryEventsApi` which is event-driven, however that simply never called back with any informtation. The `Watch` API seems to be the best I can do.
    
    Logging every frame significantly slows down the emulator, so it only logs on change.

--- OotStateExtractor.cs
+++ OotStateExtractor.cs
@@ -1,16 +1,50 @@
+using System;
 using System.Windows.Forms;
 using BizHawk.Client.Common;
+using BizHawk.Client.EmuHawk;
+using BizHawk.Emulation.Common;
+
+using DisplayType = BizHawk.Client.Common.DisplayType;
 
 namespace DevelWoutACause.OotStateExtractor {
     [ExternalTool("OoT State Extractor")]
     public sealed class Extractor : Form, IExternalToolForm {
-        [RequiredApi]
-        public IMemoryApi? memApi { get; set; }
+        [RequiredService]
+        public IMemoryDomains? memoryDomains { get; set; }
+
+        private Watch? watch;
 
         public bool AskSaveChanges() => true;
 
-        public void Restart() {}
+        public void Restart() {
+            if (memoryDomains == null) {
+                throw new Exception("Memory domains is not available.");
+            }
 
-        public void UpdateValues(ToolFormUpdateType type) {}
+            watch = Watch.GenerateWatch(
+                memoryDomains.MainMemory,
+                0x11A670 /* address */,
+                WatchSize.DWord,
+                DisplayType.Hex,
+                true /* big endian */
+            );
+        }
+
+        private string lastValue = "";
+        public void UpdateValues(ToolFormUpdateType type) {
+            // Only execute after a frame.
+            if (type != ToolFormUpdateType.PostFrame) return;
+
+            if ((object?) watch == null) {
+                throw new Exception("Watch not initialized.");
+            }
+
+            watch.Update(GlobalWin.Config.RamWatchDefinePrevious);
+            if (lastValue != watch.ValueString) {
+                Console.WriteLine(
+                    $"Address {watch.AddressString} has value {watch.ValueString}.");
+                lastValue = watch.ValueString;
+            }
+        }
     }
 }
