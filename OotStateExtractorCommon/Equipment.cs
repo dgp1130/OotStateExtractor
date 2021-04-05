a497fefb6be8f626a2870f6c912431ae23d5a8cb
Doug Parker <dgp1130@users.noreply.github.com>
2021-04-04 22:23:10 -0700

    Adds `Equipment` model to `SaveContext`.
    This class includes equipment should as swords, shields, tunics, and boots.

---
+++ OotStateExtractorCommon/Equipment.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace DevelWoutACause.OotStateExtractor.Common {
+    /**
+     * Owned equipment. "Equipment" in this context refers to swords, shields,
+     * tunics, and boots. Notably does **not** include items like sticks, bombs,
+     * slingshot, etc.
+     */
+    public sealed class Equipment {
+        [JsonProperty("has_kokiri_sword")]
+        public bool HasKokiriSword { get; init; }
+
+        [JsonProperty("has_deku_shield")]
+        public bool HasDekuShield { get; init; }
+    }
+}
--- OotStateExtractorCommon/SaveContext.cs
+++ OotStateExtractorCommon/SaveContext.cs
@@ -3,10 +3,14 @@ using Newtonsoft.Json;
 namespace DevelWoutACause.OotStateExtractor.Common {
     /** Overall save context for the game containing all relevant game state. */
     public sealed record SaveContext(
+        [property:JsonProperty("equipment")] Equipment Equipment,
         [property:JsonProperty("upgrades")] Upgrades Upgrades
     ) {
         public static SaveContext Empty() {
-            return new SaveContext(Upgrades: new Upgrades());
+            return new SaveContext(
+                Equipment: new Equipment(),
+                Upgrades: new Upgrades()
+            );
         }
     }
 }
